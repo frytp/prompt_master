b530500f72872f30312141fdf116d670fa854188
frytp <iligomzhin@gmail.com>
2026-04-03 19:18:19 +0300

    add: custom template tags for URL parameter handling

---
+++ prompts/templatetags/prompt_tags.py
@@ -0,0 +1,37 @@
+"""
+Custom template tags for prompt management.
+"""
+from django import template
+from urllib.parse import urlencode
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def url_with_params(context, **kwargs):
+    """
+    Generate URL with current query parameters plus new ones.
+    
+    Usage: {% url_with_params page=2 %}
+    """
+    query = context['request'].GET.copy()
+    for key, value in kwargs.items():
+        query[key] = value
+    return '?' + urlencode(query)
+
+
+@register.simple_tag(takes_context=True)
+def pagination_url(context, page_num):
+    """
+    Generate pagination URL preserving filters.
+    
+    Args:
+        context: Template context
+        page_num: Page number to navigate to
+        
+    Returns:
+        str: URL with pagination and filters
+    """
+    params = context['request'].GET.copy()
+    params['page'] = page_num
+    return '?' + urlencode(params)
